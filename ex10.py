a293c867c88267697cb6004533a7ffa0d2a23641
paulapelegri <ppelegri20735@iesjoanramis.org>
2023-10-24 13:20:59 +0200

    ex10

---
+++ ex10.py
@@ -0,0 +1,12 @@
+#Definició de la funció
+def major(a):
+    if a>18: 
+        print("És major d'edat")
+    elif x<18:
+        print("És menor d'edat")
+    else: 
+        print("Té 18 anys")
+
+#Programa principal
+x = int(input("Introdueixi la seva edat: "))
+major(x)
