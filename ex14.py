5eb04bbb4bcc0b477a9177664963fd2b8798504f
paulapelegri <ppelegri20735@iesjoanramis.org>
2023-12-04 11:35:49 +0100

    ex14

--- ex14.py
+++ ex14.py
@@ -1,28 +1,33 @@
 def gran_de_tres(a,b,c):
     if a>b>c:
         return a
-    elif c>a>b:
-        return c
-    elif b>c>a:
-        return b
-    elif c==a>b:
-        return c
+    elif a>b==c:
+        return a
+    elif a>c>b:
+        return a
     elif a==b>c:
-        return a 
+        return a
+    elif b>a>c:
+        return b 
     elif a==b==c:
         return b 
-    elif a<b>c:
+    elif b>c>a:
         return b
-    elif
+    elif b>a==c:
+        return b
+    elif c>a>b:
+        return c
+    elif c>b>a:
+        return c
+    elif c>a==b:
+        return c 
     
-
-
-    #Programa principal
-    x=int(input("introdueixi un número: "))
-    y=int (input("Introdueixi el segon número: "))
-    z=int(input("Introdueixi el tercer número"))
-    gran_de_tres(a,b,z)
-    print("El número més gran és: ".format(d))
+#Programa principal
+a=int(input("introdueixi un número: "))
+b=int (input("Introdueixi el segon número: "))
+c=int(input("Introdueixi el tercer número: "))
+d= gran_de_tres (a,b,c)
+print("El número més gran és: {}".format(d))
 
 
 
